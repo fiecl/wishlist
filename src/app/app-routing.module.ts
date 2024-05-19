5dda5244064d02b84cb0fc3ab66cab46e64d83cc
angelo.bassig <angelo.bassig@pointwest.com.ph>
2024-05-19 19:00:28 +0800

    Committing router-app to wishlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductslistComponent } from './products/productslist/productslist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: FirstComponent},
+  { path: "second", component: SecondComponent},
+  { path: "contact", component: ContactComponent},
+  { path: "products", component: ProductslistComponent},
+  { path: "**", component: NotFoundComponent},
+]; // These paths are all relative to the application where root: localhost:4200. So we have localhost:4200/first and localhost:4200/second
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
